114235a63ab2b1b30cfb5a89bcfe3b3e96a72810
Thiago Gama <thiagoddcqg@gmail.com>
2022-09-25 11:43:27 -0300

    Install React

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import React, { Component } from 'react';
-import api from './api';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-
-  state = {
-    filmes: [],
-  }
-
-  async componentDidMount() {
-    const response = await api.get('star%20wars');
-
-    this.setState({ filmes: response.data });
-  }
-
-  render() {
-
-    const { filmes } = this.state;
-
-    return (
-      <div>
-        <h1>Listar os Filmes</h1>
-        {filmes.map(filme => (
-          <li key={filme.show.id}>
-            <h2>
-              <strong>Título: </strong>
-              {filme.show.name}
-            </h2>
-            <p>
-              {filme.show.url}
-            </p>
-
-          </li>
-        ))}
-      </div>
-    );
-  };
-};
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;
